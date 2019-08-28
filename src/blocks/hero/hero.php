e40d12b407fdd801be736d90ec976b21c063e2fb
Jared Cuffe <jcuffe@gmail.com>
2019-08-28 16:13:46 -0700

    Injects menu-1 and footer menus into hero editor script.

--- src/blocks/hero/hero.php
+++ src/blocks/hero/hero.php
@@ -7,8 +7,11 @@ function eecontractingllc__enqueue_hero_editor_assets()
 {
   // Get nav menu data from WP database
   $theme_locations = get_nav_menu_locations();
+  $term = get_term($theme_locations['menu-1'], 'nav_menu');
+  $nav = wp_get_nav_menu_items($term);
+
   $term = get_term($theme_locations['footer'], 'nav_menu');
-  $menu = wp_get_nav_menu_items($term);
+  $hero = wp_get_nav_menu_items($term);
 
   // Enqueue editor script with injected nav menu data
   wp_register_script(
@@ -16,7 +19,10 @@ function eecontractingllc__enqueue_hero_editor_assets()
     plugins_url('hero-editor.js', __FILE__),
     ['wp-blocks', 'wp-element', 'wp-editor']
   );
-  wp_localize_script('eecontractingllc-hero-editor', 'nav', $menu);
+
+  wp_localize_script('eecontractingllc-hero-editor', 'navMenu', $nav);
+  wp_localize_script('eecontractingllc-hero-editor', 'heroMenu', $hero);
+
   wp_enqueue_script('eecontractingllc-hero-editor');
 
   wp_enqueue_style(
