3695cad7cb4ce26e77b30af50b9011d3a521a064
Jared Cuffe <jcuffe@gmail.com>
2019-08-28 16:13:07 -0700

    Implements processing method for php nav-menu data.

--- src/common/js/util.js
+++ src/common/js/util.js
@@ -26,3 +26,34 @@ export const accessibleImage = (src, alt) => {
   // No alt set, so let's hide it from screen readers
   return <img src={src} alt="" aria-hidden="true" />
 }
+
+export const processNav = nav => {
+  const items = {}
+  nav.forEach(({ ID, title, url, menu_item_parent }) => {
+    if (menu_item_parent == "0") {
+      items[ID] = { title, url, children: [] }
+    }
+  })
+  nav.forEach(({ title, url, menu_item_parent }) => {
+    if (menu_item_parent != "0") {
+      items[menu_item_parent].children.push({ title, url })
+    }
+  })
+
+  const navMenuElements = Object.values(items).map(item => (
+    <li>
+      <a href={item.url}>{item.title}</a>
+      {item.children.length ? (
+        <ul>
+          {item.children.map(child => (
+            <li>
+              <a href={child.url}>{child.title}</a>
+            </li>
+          ))}
+        </ul>
+      ) : null}
+    </li>
+  ))
+
+  return navMenuElements
+}
